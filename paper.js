0c97d2c74eef225390eb46cc5d5a78aa0208c168
Vancho52 <smetanca52ivan@gmail.com>
2018-07-17 17:40:40 +0300

    Adding all resourses on my simple game

---
+++ paper.js
@@ -0,0 +1,40 @@
+function rock() {
+  let weapons = ['ROCK', 'PAPER', 'SCISSORS'];
+  let weapon = weapons[Math.floor(Math.random()*weapons.length)];
+  if (weapon == 'ROCK') {
+    return document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
+  }
+  else if (weapon == 'PAPER') {
+    return document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
+  }
+  else {
+    return document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
+  }
+}
+
+function paper() {
+  let weapons = ['ROCK', 'PAPER', 'SCISSORS'];
+  let weapon = weapons[Math.floor(Math.random()*weapons.length)];
+  if (weapon == 'ROCK') {
+    return document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
+  }
+  else if (weapon == 'PAPER') {
+    return document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
+  }
+  else {
+    return document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
+  }
+}
+function scissors() {
+  let weapons = ['ROCK', 'PAPER', 'SCISSORS'];
+  let weapon = weapons[Math.floor(Math.random()*weapons.length)];
+  if (weapon == 'ROCK') {
+    return document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
+  }
+  else if (weapon == 'PAPER') {
+    return document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
+  }
+  else {
+    return document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
+  }
+}
