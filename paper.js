ecc9b35eee7c8535f0d947df0922e59b9b86b645
Smetanca52 <40578129+Smetanca52@users.noreply.github.com>
2018-07-17 21:35:16 +0300

    Update paper.js

--- paper.js
+++ paper.js
@@ -2,13 +2,16 @@ function rock() {
   let weapons = ['ROCK', 'PAPER', 'SCISSORS'];
   let weapon = weapons[Math.floor(Math.random()*weapons.length)];
   if (weapon == 'ROCK') {
-    return document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
+    document.getElementById("result2").innerHTML = 'Tie!';
+    document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
   }
   else if (weapon == 'PAPER') {
-    return document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
+    document.getElementById("result2").innerHTML = 'You lost!';
+    document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
   }
   else {
-    return document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
+    document.getElementById("result2").innerHTML = 'You won!';
+    document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
   }
 }
 
@@ -16,25 +19,31 @@ function paper() {
   let weapons = ['ROCK', 'PAPER', 'SCISSORS'];
   let weapon = weapons[Math.floor(Math.random()*weapons.length)];
   if (weapon == 'ROCK') {
-    return document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
+    document.getElementById("result2").innerHTML = 'You won!';
+    document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
   }
   else if (weapon == 'PAPER') {
-    return document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
+    document.getElementById("result2").innerHTML = 'Tie!';
+    document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
   }
   else {
-    return document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
+    document.getElementById("result2").innerHTML = 'You lost!';
+    document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
   }
 }
 function scissors() {
   let weapons = ['ROCK', 'PAPER', 'SCISSORS'];
   let weapon = weapons[Math.floor(Math.random()*weapons.length)];
   if (weapon == 'ROCK') {
-    return document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
+    document.getElementById("result2").innerHTML = 'You lost!';
+    document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
   }
   else if (weapon == 'PAPER') {
-    return document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
+    document.getElementById("result2").innerHTML = 'You won!';
+    document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
   }
   else {
-    return document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
+    document.getElementById("result2").innerHTML = 'Tie!';
+    document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
   }
 }
