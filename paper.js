82a5cc2bed4c681c00e1667d6fd7ffc37f6f2ab4
Vancho52 <smetanca52ivan@gmail.com>
2018-08-30 11:12:32 +0300

    Improved style and logic

--- paper.js
+++ paper.js
@@ -1,49 +1,76 @@
-function rock() {
-  let weapons = ['ROCK', 'PAPER', 'SCISSORS'];
-  let weapon = weapons[Math.floor(Math.random()*weapons.length)];
-  if (weapon == 'ROCK') {
-    document.getElementById("result2").innerHTML = 'Tie!';
-    document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
-  }
-  else if (weapon == 'PAPER') {
-    document.getElementById("result2").innerHTML = 'You lost!';
-    document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
-  }
-  else {
-    document.getElementById("result2").innerHTML = 'You won!';
-    document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
+let playerPoints = 0;
+let compPoints = 0;
+
+
+function computerPlay(){
+  choices = ["Rock", "Paper", "Scissors"];
+  let index = Math.floor(Math.random() * 3);
+  return choices[index];
+}
+
+function playOne(playerSelection, computerSelection){
+  if(playerSelection === computerSelection){
+    return "Tie!";
+
+  }else if (playerSelection == "Rock") {
+    if(computerSelection=="Scissors"){
+      return "Point!";
+    }
+    if(computerSelection=="Paper"){
+      return "Loss!";
+    }
+
+  }else if (playerSelection == "Paper"){
+    if(computerSelection=="Scissors"){
+      return "Loss!";
+    }
+    if(computerSelection=="Rock"){
+      return "Point!";
+    }
+
+  }else if (playerSelection == "Scissors"){
+    if(computerSelection=="Rock"){
+      return "Loss!";
+    }
+    if(computerSelection=="Paper"){
+      return "Point!";
+    }
   }
 }
 
-function paper() {
-  let weapons = ['ROCK', 'PAPER', 'SCISSORS'];
-  let weapon = weapons[Math.floor(Math.random()*weapons.length)];
-  if (weapon == 'ROCK') {
-    document.getElementById("result2").innerHTML = 'You won!';
-    document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
+function startRound(clicked_id){
+  let resultMessage = "";
+  if(playerPoints < 5 && compPoints < 5){
+    let playerSelection = clicked_id;
+    let computerSelection = computerPlay();
+    let value = playOne(playerSelection, computerSelection);
+    resultMessage += value + " ";
+    if(value === "Loss!"){
+      compPoints++;
+      resultMessage += ("Computer selected: " + computerSelection + ". You selected: " + playerSelection + "!");
+    }else if (value === "Point!") {
+      playerPoints++;
+      resultMessage += ("Computer selected: " + computerSelection + ". You selected: " + playerSelection + "!");
+    }else if (value === "Tie!"){
+      resultMessage += "Both selected " + playerSelection;
+    }
+    console.log("Player points: " + playerPoints + " Computer Points: " + compPoints);
   }
-  else if (weapon == 'PAPER') {
-    document.getElementById("result2").innerHTML = 'Tie!';
-    document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
+
+  if(playerPoints == 5){
+    resultMessage = "You win! Congratulations!";
+  }else if(compPoints == 5){
+    resultMessage = "You lose! Sorry :(";
   }
-  else {
-    document.getElementById("result2").innerHTML = 'You lost!';
-    document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
-  }
+  document.getElementById("result").innerHTML = compPoints;
+  document.getElementById("result2").innerHTML = playerPoints;
+  document.getElementById("result3").innerHTML = resultMessage;
 }
-function scissors() {
-  let weapons = ['ROCK', 'PAPER', 'SCISSORS'];
-  let weapon = weapons[Math.floor(Math.random()*weapons.length)];
-  if (weapon == 'ROCK') {
-    document.getElementById("result2").innerHTML = 'You lost!';
-    document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
-  }
-  else if (weapon == 'PAPER') {
-    document.getElementById("result2").innerHTML = 'You won!';
-    document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
-  }
-  else {
-    document.getElementById("result2").innerHTML = 'Tie!';
-    document.getElementById("result").innerHTML = 'Computer chose: ' + weapon;
-  }
+
+function reset(){
+  playerPoints = 0;
+  compPoints = 0;
+  document.getElementById("result").innerHTML = compPoints;
+  document.getElementById("result2").innerHTML = playerPoints;
+  document.getElementById("result3").innerHTML = "";
 }
